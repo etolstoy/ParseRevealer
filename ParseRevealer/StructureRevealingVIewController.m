0d4909b6819dfdee93f2240f5e99a899f5478d4d
Egor Tolstoy <igrekde@gmail.com>
2015-03-23 22:17:34 +0300

    Added a new view controller for structure revealing

---
+++ ParseRevealer/StructureRevealingVIewController.m
@@ -0,0 +1,22 @@
+//
+//  StructureRevealingVIewController.m
+//  ParseRevealer
+//
+//  Created by Egor Tolstoy on 23.03.15.
+//  Copyright (c) 2015 Egor Tolstoy. All rights reserved.
+//
+
+#import "StructureRevealingVIewController.h"
+
+@interface StructureRevealingVIewController ()
+
+@end
+
+@implementation StructureRevealingVIewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do view setup here.
+}
+
+@end
